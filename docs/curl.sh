5a8b41ca246433bf44156fecdc0c44d964ef88c7
Alexander R Torrijos <artorrijos@gmail.com>
2024-03-30 15:09:59 +0800

    Added sample curl request

---
+++ docs/curl.sh
@@ -0,0 +1,4 @@
+curl 'http://localhost:5000/' \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -H 'X-Api-Key: _ic_9fad2b4be649887c70a58b869c8838075b0dcf91554e64e2b367ba3079d079f5_fea'
