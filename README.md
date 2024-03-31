01516c690169f9fefcca7de9cd09673cbd6e88fd
Alexander R Torrijos <artorrijos@gmail.com>
2024-03-31 20:06:36 +0800

    Client Access from Spa Server working

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # ItemCatalogue
 
+## Setup
+
+
+When Testing in Swagger or .http use the following:
+
+`X-Api-Key` value `_ic_9fad2b4be649887c70a58b869c8838075b0dcf91554e64e2b367ba3079d079f5_fea`
+
+`catalogueId` value `88bd0000-f588-04d9-3c54-08dc4e40d836`
+
 ## Add Migration
 
 ```bash
--- src/ItemCatalogue.Spa.Angular/ItemCatalogue.Spa.Angular.Server/Program.cs
+++ src/ItemCatalogue.Spa.Angular/ItemCatalogue.Spa.Angular.Server/Program.cs
@@ -1,9 +1,31 @@
+using System.Diagnostics;
+
+using ItemCatalogue.Spa.Angular.Server.Clients;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var authentication = builder.Configuration.GetSection("ItemCatalogueApi:Authentication").Get<Authentication>();
+ArgumentNullException.ThrowIfNull(authentication);
+
+builder.Services.AddHttpClient<IItemCatalogueApiClient, ItemCatalogueApiClient>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("ItemCatalogueApi:BaseUrl")!);
+    client.DefaultRequestHeaders.Add(authentication.Name, authentication.Value);
+});
+
+builder.Services.Configure<CatalogueId>(builder.Configuration.GetSection("ItemCatalogueApi:CatalogueId"));
+
+builder.Services.AddProblemDetails();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument();
+
+
 
 var app = builder.Build();
 
@@ -22,8 +44,12 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", async (IItemCatalogueApiClient client, IOptions<CatalogueId> options) =>
 {
+    var response = await client.GetCatalogueItemsAsync(options.Value.Value);
+
+    Debug.WriteLine($"CatalogueName: {response.Name}");
+
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
