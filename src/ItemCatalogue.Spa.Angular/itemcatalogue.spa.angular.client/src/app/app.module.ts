dfbf645b5f1f85617e85d88526675fc2bc5be19e
Alexander R Torrijos <artorrijos@gmail.com>
2024-04-03 17:31:25 +0800

    Fix for theming

--- src/ItemCatalogue.Spa.Angular/itemcatalogue.spa.angular.client/src/app/app.module.ts
+++ src/ItemCatalogue.Spa.Angular/itemcatalogue.spa.angular.client/src/app/app.module.ts
@@ -10,8 +10,10 @@ import { ItemComponent } from './catalog/item/item.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [AppComponent, CatalogComponent, ItemComponent],
   imports: [
@@ -23,6 +25,8 @@ import { RouterModule, RouterOutlet } from '@angular/router';
     RouterOutlet,
     RouterModule,
     MatCardModule,
+    MatSidenavModule,
+    MatButtonModule,
   ],
   providers: [CatalogueClient, provideAnimationsAsync()], // Add the missing module to the providers array
   bootstrap: [AppComponent],
